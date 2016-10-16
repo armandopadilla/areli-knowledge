f7aa3b7f9df88c776bbdb8a2d9fe45b34f28200f
armandopadilla <armando.padilla@dws.la>
2016-10-16 20:26:20 +0000

    integration of wikipedia

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+
+
+// Wolfram Alpha
 var APPID = "5HW2GQ-AQ3U8978VQ";
 var wolfram = require('wolfram-alpha').createClient(APPID);
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // for parsing application/json
+var request = require('request');
 
 app.post('/v1/areli-knowledge', function(req, res){
   
@@ -13,28 +17,78 @@ app.post('/v1/areli-knowledge', function(req, res){
   var action = jsonPayload.result.action;
 
   if (action === 'get.city.time') {
-	var city = jsonPayload.result.parameters.city;
-	getTime(city, function(err, time){
 
-		var response = {
+    var city = jsonPayload.result.parameters.city;
+    getTime(city, function(err, time) {
+
+      var response = {
         	"speech": "It's currently "+time+" in "+city,
         	"displayText": "It's currently "+time+" in "+city,
         	"data": "It's currently "+time+" in "+city,
         	"contextOut": [],
         	"source": "Areli"
-  		}	
+  		};
 
-	
-		res.json(response);
+		  res.json(response);
 
-	});
-  }
-  else {
-    //do something
+    });
+  } else if(action === 'get.people.info') {
+
+    var person = jsonPayload.result.parameters.person;
+    getPeopleInfo(person, function(err, summary) {
+
+      var response = {
+        "speech": summary,
+        "displayText": summary,
+        "data": summary,
+        "contextOut": [],
+        "source": "Areli"
+      };
+
+      res.json(response);
+
+    });
+
   }
 
 
 });
+
+/**
+ * Get info on a specific person in history.
+ * Or that
+ */
+function getPeopleInfo(person, cb) {
+
+  if (person == '') return cb('Could not find info.');
+
+  //Clean the name.  Set to lower case then capitalizethe worlds
+  var cleanedName = '';
+  person = person.toLowerCase();
+  var personWords = person.split(" ");
+  personWords.forEach(function(word){
+    cleanedName += word.charAt(0).toUpperCase() + word.slice(1, word.length)+" ";
+  });
+
+  var API_URL = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&explaintext=&&titles='+encodeURI(cleanedName);
+  request(API_URL, function(err, response, data){
+
+    if (err) return cb(err);
+    if (data) {
+      var json = JSON.parse(data);
+      var pages = json.query.pages;
+      var summary = '';
+      Object.keys(pages).forEach(function(key) {
+        summary = pages[key].extract;
+      });
+
+      return cb(null, summary);
+    }
+
+  });
+
+}
+
 
 /**
  * Fetch the time for a specific city.
@@ -67,14 +121,24 @@ function getTime(city, cb) {
 /**
  * used for testing...
  */
-app.get('/v1/areli-knowledge', function(req, res){
+app.get('/v1/areli-knowledge/time', function(req, res){
 
   var city = req.query.city;
-  var time = getTime(city, function(err, time){
+  getTime(city, function(err, time){
     res.send(time);
   });
 
 });
+
+app.get('/v1/areli-knowledge/people', function(req, res){
+
+  var person = req.query.person;
+  getPeopleInfo(person, function(err, summary){
+    res.send(summary);
+  });
+
+});
+
 
 
 app.listen(3001, function(){
